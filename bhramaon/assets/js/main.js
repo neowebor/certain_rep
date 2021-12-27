9b38062bff1906ddbb53c04cfbe4363bc7e79f96
Kirill <neowebor@gmail.com>
2021-12-27 18:00:42 +0200

    fix

--- bhramaon/assets/js/main.js
+++ bhramaon/assets/js/main.js
@@ -1,6 +1,5 @@
 $(function(){
     $(window).scroll(function(){
-        console.log($(window).scrollTop());
         if($(window).scrollTop()>766) {
             $("header").addClass("fixed");
         }else{
