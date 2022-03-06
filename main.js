824080f39c4ac1ea7dfcdbaf7257112637a662f7
Kirill <neowebor@gmail.com>
2022-03-06 17:49:05 +0200

    fix

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function showDescription() {
     const showText = document.querySelector('.section__about')
     const showSkills = document.querySelector('.section__skills');
     const projects = document.querySelector('.section__projects');
+    const my_info = document.querySelector('.section__my-info');
 
 
     elem.addEventListener('click', (e) => {
@@ -12,7 +13,9 @@ function showDescription() {
             showSkills.classList.toggle('active');
         } else if(e.target.closest('.projects')) {
             projects.classList.toggle('active');
-        }    
+        } else if(e.target.closest('.my-info')) {
+            my_info.classList.toggle('active')
+        }
     })  
 
 }
